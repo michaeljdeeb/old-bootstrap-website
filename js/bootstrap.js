b213ed29c5f920f609d534556286890313c458ec
Michael Deeb <michaeljdeeb@gmail.com>
2015-06-22 02:39:35 -0700

    Out with the hamburger, and in with an animated arrow

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -609,6 +609,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   // =================
 
   $(document).on('click.bs.collapse.data-api', '[data-toggle=collapse]', function (e) {
+    $('.navbar-toggle').toggleClass('ease-arrow');
     var $this   = $(this), href
     var target  = $this.attr('data-target')
         || e.preventDefault()
